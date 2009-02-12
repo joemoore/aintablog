828a7477793a951a4857e4fa7ea324240b0247bc
Joseph Moore <joe.moore@gmail.com>
2009-02-11 22:12:23 -0800

    adding application.rb for rails 2.2.2 goodness

---
+++ app/controllers/application.rb
@@ -0,0 +1,43 @@
+# Filters added to this controller apply to all controllers in the application.
+# Likewise, all the methods added will be available for all controllers.
+
+class ApplicationController < ActionController::Base
+  include AuthenticatedSystem
+  protect_from_forgery :secret => 'dd49bc1096180f8639bde09ce072c108'
+  # protect_from_forgery
+  rescue_from ActiveRecord::RecordNotFound, :with => :not_found
+  
+  helper :all # include all helpers, all the time
+  
+  filter_parameter_logging :password, :password_confirmation
+  
+  def current_post
+    @current_post ||= Post.find_by_permalink(params[:id], :include => :comments) || Post.find(params[:id])
+  end
+  
+  protected 
+  
+  def post_repo
+    begin
+      @post_type = post_type.to_s.tableize
+      @post_type.classify.constantize
+    rescue => e
+      logger.info(e)
+      @post_type = :posts
+      retry
+    end
+  end
+
+  def not_found
+    cookies[:error] = "Sorry but that post could not be found."
+    redirect_to root_path and return
+  end
+  
+  private
+  
+  def expire_path(file)
+    file = File.join(Rails.root.to_str, 'public', file)
+    FileUtils.rm_rf(file) if File.exists?(file)
+    logger.info("Expired cache: #{file}")
+  end
+end
