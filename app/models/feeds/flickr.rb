7c064514d99aeab8eb92bdc34051e11cbe90a47d
Joseph Moore <joe.moore@gmail.com>
2009-02-28 12:25:23 -0800

    jlm: flickr feeds look better, blockquote description in both posts and rss

--- app/models/feeds/flickr.rb
+++ app/models/feeds/flickr.rb
@@ -3,11 +3,11 @@ class Flickr < Feed
   entries_become :pictures do |picture, entry|
     picture.cite = entry.urls.first
     picture.header = entry.title
-    picture.content = entry.description
     picture.permalink = (Hpricot(entry.content)/"img").first['src'].gsub(/_m/, '')
+    description = (Hpricot.parse(entry.content)/"p")[2]
+    picture.content = description.to_html if description
     picture.created_at = entry.try(:date_published)
     picture.updated_at = entry.try(:last_updated)
     picture.save
-  end
-  
+  end  
 end
