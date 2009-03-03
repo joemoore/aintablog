a458521dcb142271ca3f82475a545715efa2c503
Joseph Moore <joe.moore@gmail.com>
2009-03-02 21:40:41 -0800

    merging 0040-tweets-constructed-with-a-header-up-to-the-first-5.patch

--- app/models/feeds/twitter.rb
+++ app/models/feeds/twitter.rb
@@ -1,7 +1,12 @@
 class Twitter < Feed
   
   entries_become :tweets do |tweet, entry|
-    tweet.content = entry.content.gsub(/\A\w*:?\s/, '')
+    content = entry.content.gsub(/\A\w*:?\s/, '')
+    tweet.content = content
+    words = content.split(' ')
+    header = words[0..5].join(' ')
+    tweet.header = header
+    tweet.header << ' ...' unless header.ends_with?('.')
     tweet.permalink = entry.urls.first
     tweet.created_at = entry.try(:date_published)
     tweet.save ? tweet : tweet.destroy
